06c146ad13e3f81de4cd010582d5edd52131e6cc
Chandana-NM-2025 <chandananmgowda1616@gmail.com>
2026-09-03 14:59:14 +0530

    Revert "[chandana] refactor: remove configuration management components and types"
    This reverts commit a3b461f9dac226d052866750598e2486e20ebc51.

---
+++ frontend/src/app/(main)/admin/configuration/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import ConfigurationsListPage from "@/features/configuration/components/ConfigurationsListPage";
+
+export default function Page() {
+  return <ConfigurationsListPage />;
+}
